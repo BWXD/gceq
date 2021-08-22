152422ece2dd4f19b4d82ed8047c4fcbc540f5bd
BWXD <44984123+BWXD@users.noreply.github.com>
2021-08-22 18:56:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 var client = new Discord.Client();
 
 var room = "https://discord.gg/rpxjesCRg8";
@@ -59,3 +59,70 @@ switch (args[0].toLowerCase()) {
 });
 
 client.login(process.env.TOKEN);
+*/
+
+const Discord = require("discord.js");
+var client = new Discord.Client();
+
+var room = "";
+
+// BOT ONLINE
+
+client.on("ready", function() {
+        var dd = new Date;
+        var h = dd.getUTCHours();
+        var m = dd.getUTCMinutes();
+        var hh = ""; var mm = "";
+        if(h < 10){hh = "0"} else if(h > 9){hh = ""}
+        if(m < 10){mm = "0"} else if(m > 9){mm = ""}
+        console.log("Bot is up.")
+        client.channels.cache.get("876579128692834364").send(new Discord.MessageEmbed()
+        .setColor("#F2F2F2")
+        .setTitle("Bot was deployed at __" + hh + h + ":" + mm + m + " GMT__."))
+        client.user.setStatus("online")
+});
+
+
+client.on("message", async function(message) {
+if (message.author.equals(client.user)) return;
+
+
+if (!message.content.startsWith("!")) return;
+var args = message.content.substring("!".length).split(" ");
+switch (args[0].toLowerCase()) {
+
+    case "help":
+        message.reply("!help")
+    break;
+
+
+    case "login":
+        message.reply("!login")
+    break;
+
+    case "room":
+        if(message.author.id == "634872299069374488" && message.content.startsWith("!room https://www.haxball.com/play")){
+            const emb = new Discord.MessageEmbed()
+                .setColor("#14AF34")
+                .setTitle(message.content.split("!room "))
+            client.channels.cache.get("879046204011446312").bulkDelete(20)
+            client.channels.cache.get("879046204011446312").send(emb)
+            room = message.content.split("!room ");
+        }
+        message.delete();
+    break;
+
+    case "say":
+        if(message.author.id == "634872299069374488"){
+            message.delete();
+            const emb = new Discord.MessageEmbed()
+                .setColor("#14AF34")
+                .setTitle(message.content.split("!say "))
+            message.channel.send(emb)
+        }
+    break;
+
+    }
+});
+
+client.login("ODc2NTc1NTQ5Nzc3MTQ5OTcy.YRmEmQ.W-6aifH2tArH23USSgtdvdfaoRE");
